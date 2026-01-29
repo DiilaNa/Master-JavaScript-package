0c0cfb498ece0c8106149cc1214a6fbe35183a98
DiilaNa <liyanaarachchidilan@gmail.com>
2026-01-29 14:36:32 +0530

    variable types

---
+++ Basics/variable-types.js
@@ -0,0 +1,21 @@
+// Variable Types in JavaScript
+
+// var - use to declare global-scoped variables
+// can reassign and redeclare
+var y = 15
+var y = 25;
+
+console.log(y);
+
+// let - use to declare block-scoped variables
+// can reassign but cannot redeclare
+let x = 5
+// let x = 25;
+console.log(x);
+
+// const - use to declare variables that won't be reassigned
+const a = 10
+console.log(a);
+
+// if we assign a value to a const , it shows a TypeError
+
