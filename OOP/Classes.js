28289f219c812908482e347312a6dfc01f0a7d2c
DiilaNa <liyanaarachchidilan@gmail.com>
2026-02-01 13:19:15 +0530

    updated classes logic-OOP

--- OOP/Classes.js
+++ OOP/Classes.js
@@ -13,5 +13,18 @@ class Animal {
     }
 }
 
+class Dog extends Animal{
+    speak(){
+        console.log(this.type+' '+ 'barks');
+        
+    }
+}
+
 const animal = new Animal('Animal','regular','1')
 console.log(animal);
+
+const dog = new Dog('Rex','Dog','2')
+animal.speak();
+dog.speak();
+
+
